cd381d878f0cb008a6f5f42be6e0d73bbee4d320
alex <alexhsu.iisi@gmail.com>
2016-05-23 23:24:15 +0800

    Update code

--- src/main/java/exam07/CounterTest.java
+++ src/main/java/exam07/CounterTest.java
@@ -1,5 +1,14 @@
 package exam07;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CounterTest {
 
+	@Test
+	public void testBasicFunctionality() throws Exception{
+		Counter counter = new Counter();
+		Assert.assertEquals(0, counter.value());
+		
+	}
 }
